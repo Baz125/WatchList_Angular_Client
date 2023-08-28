ae9a5e06196611b92b43d1e4e1099ffebc8863ad
Baz125 <103266154+Baz125@users.noreply.github.com>
2023-08-28 07:52:36 +0100

    added genre dialog

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieDataApisService } from '../services/movie-data-apis.service'
-
+import { GenreDialogComponent } from '../genre-dialog/genre-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,17 +10,26 @@ import { MovieDataApisService } from '../services/movie-data-apis.service'
 })
 export class MovieCardComponent {
   movies: any[] = [];
-  constructor(public fetchApiData: MovieDataApisService) { }
+  constructor(
+    public fetchApiData: MovieDataApisService,
+    public dialog: MatDialog) { }
 
-ngOnInit(): void {
-  this.getMovies();
-}
 
-getMovies(): void {
-  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
-      this.movies = resp;
-      console.log(this.movies);
-      return this.movies;
-    });
+  ngOnInit(): void {
+    this.getMovies();
   }
+
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+        this.movies = resp;
+        return this.movies;
+      });
+    }
+
+  onGenreClick(): void {
+    this.dialog.open(GenreDialogComponent {
+      width: '280px'
+    });
+  };
+
 }
